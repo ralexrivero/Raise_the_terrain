39a99f5ce38dfac46420f29a58507f74e1366a1e
ralexrivero <ralexrivero@gmail.com>
2021-09-19 16:20:34 -0400

    apply structure

--- w_ini_instance.c
+++ w_ini_instance.c
@@ -9,20 +9,20 @@ int init_instance(SDL_Instance *instance)
 		return (1);
 	}
 	/* Create a new Window instance */
-	window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDOWPOS_CENTERED,
+	instance->window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
-	if (window == NULL)
+	if (instance->window == NULL)
 	{
 		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
 	}
 	/* Create a new Renderer instance linked to the window */
-	renderer = SDL_CreateRenderer(window, -1,
+	instance->renderer = SDL_CreateRenderer(instance->window, -1,
 		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == NULL)
+	if (instance->renderer == NULL)
 	{
-		SDL_DestroyWindow(window);
+		SDL_DestroyWindow(instance->window);
 		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
