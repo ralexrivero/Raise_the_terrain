18f0e66f9b0cd863a23f5b01a3e81a9a31961160
ralexrivero <ralexrivero@gmail.com>
2021-09-19 13:40:04 -0400

    initilize SDL2

---
+++ sdl_init.c
@@ -0,0 +1,16 @@
+#include <SDL2/SDL.h>
+
+int main(void)
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+
+	/* Initilize SDL */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		frintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		return (1);
+	}
+
+	return (0);
+}
